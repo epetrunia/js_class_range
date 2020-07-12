3fc8b28e3c0a77678ba3f154eb7efdb4b1655e3c
Eugenia Petrunia <avepetrunia@gmail.com>
2020-07-12 09:57:37 +0300

    Create RangeValidator class

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 'use strict';
+
+class RangeValidator {
+    constructor(from = 0, to = 0) {
+        this._from = from;
+        this._to = to;
+    }
+
+    set from(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('value must be a number')
+        }
+
+        this._from = value;
+    }
+
+    get from() {
+        return this._from;
+    }
+
+    set to(value) {
+        if (typeof value !== 'number') {
+            throw new TypeError('value must be a number')
+        }
+
+        this._to = value;
+    }
+
+    get to() {
+        return this._to;
+    }
+
+    get range() {
+        const rangeArray = [];
+        rangeArray.push(this._from, this._to);
+        return rangeArray;
+    }
+
+    value(num) {
+        if (typeof num !== 'number') {
+            throw new TypeError('value must be a number')
+        }
+
+        return (num >= this._from && num <= this._to);
+    }
+}
+
+const range1 = new RangeValidator(5, 10);
+console.log(range1.range);
